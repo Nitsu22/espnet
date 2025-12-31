a9a337c8e94650204825fb69733a123ca3f8e9a8
Nitsu22 <nitsu@ks.c.titech.ac.jp>
2026-01-01 01:34:38 +0900

    Update run_small_sp.sh and training configuration for enhanced model execution
    - Changed CUDA device from 5 to 0 in run_small_sp.sh for improved resource allocation.
    - Updated the enhancement experiment path in run_small_sp.sh for better organization.
    - Adjusted the stop stage from 8 to 6 in run_small_sp.sh to streamline the training process.
    - Modified the spatial encoder model path in train_enh_tflocoformer_small_sp.yaml for correct file referencing.

--- egs2/whamr/enh2/run_small_sp.sh
+++ egs2/whamr/enh2/run_small_sp.sh
@@ -14,7 +14,7 @@ train_set=tr_mix_both_reverb_${min_or_max}_${sample_rate}
 valid_set=cv_mix_both_reverb_${min_or_max}_${sample_rate}
 test_sets="tt_mix_both_reverb_${min_or_max}_${sample_rate}"
 
-CUDA_VISIBLE_DEVICES=5 ./enh_sp.sh \
+CUDA_VISIBLE_DEVICES=0 ./enh_sp.sh \
     --train_set "${train_set}" \
     --valid_set "${valid_set}" \
     --test_sets "${test_sets}" \
@@ -23,11 +23,11 @@ CUDA_VISIBLE_DEVICES=5 ./enh_sp.sh \
     --ref_num 2 \
     --local_data_opts "--sample_rate ${sample_rate} --min_or_max ${min_or_max}" \
     --enh_config ./conf/tuning/train_enh_tflocoformer_small_sp.yaml \
-    --enh_exp exp/enh_train_enh_tflocoformer_small_sp_repro \
+    --enh_exp exp/enh_train_enh_tflocoformer_small_sp \
     --use_dereverb_ref false \
     --use_noise_ref true \
     --inference_model "valid.loss.best.pth" \
     --audio_format wav \
     --stage 5 \
-    --stop_stage 8 \
+    --stop_stage 6 \
     "$@"
