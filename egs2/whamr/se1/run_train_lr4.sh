a3dc4630c9af494d319f20e3b4c2a791025e5833
Nitsu22 <nitsu@ks.c.titech.ac.jp>
2026-01-12 22:53:16 +0900

    Revert CUDA device allocation in run_small_se_condition_4gpu_lithium.sh back to 4-7 for optimized processing efficiency.

---
+++ egs2/whamr/se1/run_train_lr4.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+# Set bash to 'debug' mode, it will exit on :
+# -e 'error', -u 'undefined variable', -o ... 'error in pipeline', -x 'print commands',
+set -e
+set -u
+set -o pipefail
+
+min_or_max=min # "min" or "max". This is to determine how the mixtures are generated in local/data.sh.
+sample_rate=8k
+
+train_set=tr_mix_both_reverb_${min_or_max}_${sample_rate}
+valid_set=cv_mix_both_reverb_${min_or_max}_${sample_rate}
+test_sets="tt_mix_both_reverb_${min_or_max}_${sample_rate}"
+
+
+./se_reverse_train.sh \
+    --train_set "${train_set}" \
+    --valid_set "${valid_set}" \
+    --test_sets "${test_sets}" \
+    --fs ${sample_rate} \
+    --ngpu 1 \
+    --local_data_opts "--sample_rate ${sample_rate} --min_or_max ${min_or_max}" \
+    --enh_config ./conf/tuning/train_se_resnet2d_lr4.yaml \
+    --enh_exp exp/se_train_se_resnet2d_lr4 \
+    --audio_format wav \
+    --stage 6 \
+    "$@"
