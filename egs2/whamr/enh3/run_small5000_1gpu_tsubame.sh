e1fa14ed4c8c55eee239cb3a4e7ba56c3ae495c4
Nitsu22 <nitsu@ks.c.titech.ac.jp>
2026-01-18 23:34:24 +0900

    Remove deprecated enhancement scripts and update run configuration for WHAMR mixture creation to streamline training process.

--- egs2/whamr/enh3/run_small5000_1gpu_tsubame.sh
+++ egs2/whamr/enh3/run_small5000_1gpu_tsubame.sh
@@ -10,11 +10,11 @@ sample_rate=8k
 
 
 
-train_set=tr_mix_both_reverb_${min_or_max}_${sample_rate}_5000
+train_set=tr_mix_both_reverb_${min_or_max}_${sample_rate}
 valid_set=cv_mix_both_reverb_${min_or_max}_${sample_rate}
 test_sets="tt_mix_both_reverb_${min_or_max}_${sample_rate}"
 
-./enh_5000.sh \
+CUDA_VISIBLE_DEVICES=0 ./enh.sh \
     --train_set "${train_set}" \
     --valid_set "${valid_set}" \
     --test_sets "${test_sets}" \
@@ -22,10 +22,13 @@ test_sets="tt_mix_both_reverb_${min_or_max}_${sample_rate}"
     --ngpu 1 \
     --ref_num 2 \
     --local_data_opts "--sample_rate ${sample_rate} --min_or_max ${min_or_max}" \
-    --enh_config ./conf/tuning/train_enh_beamformer_mvdr.yaml \
+    --enh_config ./conf/tuning/train_enh_tflocoformer_small.yaml \
+    --enh_exp exp/enh_train_enh_tflocoformer_small_5000_1gpu \
     --use_dereverb_ref false \
     --use_noise_ref true \
     --inference_model "valid.loss.best.pth" \
     --audio_format wav \
-    --stop_stage 5 \
+    --stage 6 \
+    --stop_stage 8 \
     "$@"
+
