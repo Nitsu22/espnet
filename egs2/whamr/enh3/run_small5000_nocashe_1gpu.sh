2c9e71dcbc6ee211d651b7698e42137a7c85fa5e
Nitsu22 <nitsu@ks.c.titech.ac.jp>
2026-01-24 20:46:01 +0900

    Update run_small5000_nocashe_1gpu.sh to change stage parameter from 5 to 6, aligning with the updated training workflow.

--- egs2/whamr/enh3/run_small5000_nocashe_1gpu.sh
+++ egs2/whamr/enh3/run_small5000_nocashe_1gpu.sh
@@ -28,7 +28,7 @@ CUDA_VISIBLE_DEVICES=0 ./enh.sh \
     --use_noise_ref true \
     --inference_model "valid.loss.best.pth" \
     --audio_format wav \
-    --stage 5 \
+    --stage 6 \
     --stop_stage 8 \
     "$@"
 
