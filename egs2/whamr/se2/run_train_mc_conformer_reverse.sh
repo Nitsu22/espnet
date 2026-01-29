54602e68c804a5fd44804713dc246fcef3127af0
Nitsu22 <nitsu@ks.c.titech.ac.jp>
2026-01-29 19:51:55 +0900

    Refactor spatial encoder integration in TFLocoformer SE Condition model to improve compatibility with MC Conformer and optimize performance for multi-condition audio separation tasks.

---
+++ egs2/whamr/se2/run_train_mc_conformer_reverse.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+# Set bash to 'debug' mode, it will exit on :
+# -e 'error', -u 'undefined variable', -o ... 'error in pipeline', -x 'print commands',
+set -e
+set -u
+set -o pipefail
+
+min_or_max=min # "min" or "max". This is to determine how the mixtures are generated in local/data.sh.
+sample_rate=8k
+
+train_set=tr_mix_both_reverb_${min_or_max}_${sample_rate}
+valid_set=cv_mix_both_reverb_${min_or_max}_${sample_rate}
+test_sets="tt_mix_both_reverb_${min_or_max}_${sample_rate}"
+
+
+./se_mc_conformer_reverse.sh \
+    --train_set "${train_set}" \
+    --valid_set "${valid_set}" \
+    --test_sets "${test_sets}" \
+    --fs ${sample_rate} \
+    --ngpu 1 \
+    --local_data_opts "--sample_rate ${sample_rate} --min_or_max ${min_or_max}" \
+    --enh_config ./conf/tuning/train_se_mc_conformer_kd.yaml \
+    --expdir exp_reverse \
+    --enh_exp exp_reverse/se_train_mc_conformer_kd_reverse \
+    --audio_format wav \
+    --stage 5 \
+    --stop_stage 5 \
+    --init_param /net/midgar/work/nitsu/learning/tf-locoformer/espnet/egs2/whamr/enh3/exp/enh_train_enh_tflocoformer_se_conditon_mc_conformer_temporal_batch16_remake_hydrogen/valid.loss.ave_5best.pth:spatial_encoder:teacher_spatial_encoder \
+    "$@"
