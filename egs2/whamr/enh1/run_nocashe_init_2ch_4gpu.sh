97dd7dae93f771d7403fbb3d7c489c4f97a97f2c
Nitsu22 <nitsu@ks.c.titech.ac.jp>
2026-02-02 23:30:32 +0900

    Update enhancement script to modify experiment directory naming convention
    This commit updates the `run_nocashe_init_2ch_4gpu.sh` script to change the enhancement experiment directory from `exp/enh_train_enh_tflocoformer_nocashe_repro_4gpu` to `exp/enh_train_enh_tflocoformer_nocashe_2ch_init_4gpu`. This change reflects a more accurate naming convention for the experiment setup, improving clarity in the experiment tracking process.

--- egs2/whamr/enh1/run_nocashe_init_2ch_4gpu.sh
+++ egs2/whamr/enh1/run_nocashe_init_2ch_4gpu.sh
@@ -26,7 +26,7 @@ CUDA_VISIBLE_DEVICES=0,1,2,3 ./enh.sh \
     --ref_num 2 \
     --local_data_opts "--sample_rate ${sample_rate} --min_or_max ${min_or_max}" \
     --enh_config ./conf/tuning/train_enh_tflocoformer_nocashe.yaml \
-    --enh_exp exp/enh_train_enh_tflocoformer_nocashe_repro_4gpu \
+    --enh_exp exp/enh_train_enh_tflocoformer_nocashe_2ch_init_4gpu \
     --use_dereverb_ref false \
     --use_noise_ref true \
     --inference_model "valid.loss.best.pth" \
