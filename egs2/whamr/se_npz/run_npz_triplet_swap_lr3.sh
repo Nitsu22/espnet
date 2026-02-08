1c2f04f12b406d9e09f95b688327396e4f0b234f
Nitsu22 <nitsu@ks.c.titech.ac.jp>
2026-02-08 21:00:11 +0900

    Update inference module in `enh_se_condition.sh` to use enhanced separation condition variant
    This commit modifies the inference module assignment in the `enh_se_condition.sh` script to utilize `espnet2.bin.enh_inference_se_condition_div` for non-TSE tasks. This change aligns with recent updates to enhance model separation capabilities.

---
+++ egs2/whamr/se_npz/run_npz_triplet_swap_lr3.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+# Set bash to 'debug' mode, it will exit on :
+# -e 'error', -u 'undefined variable', -o ... 'error in pipeline', -x 'print commands',
+set -e
+set -u
+set -o pipefail
+
+min_or_max=min # "min" or "max". This is to determine how the mixtures are generated in local/data.sh.
+sample_rate=8k
+
+
+
+train_set=tr_mix_clean_reverb_${min_or_max}_${sample_rate}
+valid_set=cv_mix_clean_reverb_${min_or_max}_${sample_rate}
+test_sets="tt_mix_clean_reverb_${min_or_max}_${sample_rate}"
+
+./enh_npz.sh \
+    --train_set "${train_set}" \
+    --valid_set "${valid_set}" \
+    --test_sets "${test_sets}" \
+    --fs ${sample_rate} \
+    --ngpu 1 \
+    --ref_num 2 \
+    --local_data_opts "--sample_rate ${sample_rate} --min_or_max ${min_or_max}" \
+    --enh_config ./conf/tuning/train_se_mc_conformer_triplet_swap_noise_lr3.yaml \
+    --use_dereverb_ref false \
+    --use_noise_ref false \
+    --expdir exp_swap \
+    --enh_exp exp_swap/enh_train_se_mc_conformer_triplet_noise_lr3 \
+    --inference_model "valid.loss.best.pth" \
+    --audio_format wav \
+    --stage 6 \
+    --stop_stage 6 \
+    "$@"
