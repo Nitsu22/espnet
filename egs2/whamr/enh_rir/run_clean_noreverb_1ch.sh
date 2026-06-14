b021e74cc90f522a1b17ba0764bb9e3a405f7dc2
Nitsu22 <nitsu@ks.c.titech.ac.jp>
2026-06-14 22:25:01 +0900

    Update experiment directory name to 'exp_noreverb' in run scripts for clarity

--- egs2/whamr/enh_rir/run_clean_noreverb_1ch.sh
+++ egs2/whamr/enh_rir/run_clean_noreverb_1ch.sh
@@ -13,7 +13,7 @@ sample_rate=8k
 stage=3
 stop_stage=4
 dumpdir=dump_clean
-expdir=exp
+expdir=exp_noreverb
 
 train_set=tr_mix_clean_anechoic_${min_or_max}_${sample_rate}
 valid_set=cv_mix_clean_anechoic_${min_or_max}_${sample_rate}
