f8655403a5af74772a708401b51bdfd2850ef9f6
Nitsu22 <nitsu@ks.c.titech.ac.jp>
2026-06-30 20:35:39 +0900

    Use clean reverb dump for SepReformer base

--- egs2/whamr/enh_sepre1/run_sepreformer_base.sh
+++ egs2/whamr/enh_sepre1/run_sepreformer_base.sh
@@ -8,10 +8,11 @@ set -o pipefail
 min_or_max=min
 sample_rate=8k
 ngpu=2
+dumpdir=dump_rir_clean
 
-train_set=tr_mix_both_reverb_${min_or_max}_${sample_rate}
-valid_set=cv_mix_both_reverb_${min_or_max}_${sample_rate}
-test_sets="tt_mix_both_reverb_${min_or_max}_${sample_rate}"
+train_set=tr_mix_clean_reverb_${min_or_max}_${sample_rate}
+valid_set=cv_mix_clean_reverb_${min_or_max}_${sample_rate}
+test_sets="tt_mix_clean_reverb_${min_or_max}_${sample_rate}"
 
 ./enh_sepreformer.sh \
     --train_set "${train_set}" \
@@ -22,11 +23,12 @@ test_sets="tt_mix_both_reverb_${min_or_max}_${sample_rate}"
     --ref_num 2 \
     --local_data_opts "--sample_rate ${sample_rate} --min_or_max ${min_or_max}" \
     --enh_config ./conf/tuning/train_enh_sepreformer_base.yaml \
-    --enh_exp exp/enh_train_enh_sepreformer_base_clean \
+    --enh_exp exp/enh_train_enh_sepreformer_base_clean_reverb \
     --use_dereverb_ref false \
     --use_noise_ref false \
     --inference_model "valid.loss.best.pth" \
     --audio_format wav \
-    --stage 6 \
+    --dumpdir "${dumpdir}" \
+    --stage 5 \
     --stop_stage 8 \
     "$@"
